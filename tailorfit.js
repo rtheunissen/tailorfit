bd36ffba0c13a52529ab3a1b55ceae77002b6332
Rudi Theunisse <groenengoud@gmail.com>
2013-04-13 20:35:46 +1200

    Added a demo, combined fit methods into a single tailorfit function

--- tailorfit.js
+++ tailorfit.js
@@ -8,44 +8,52 @@
  * @version 1.0.0
  */
 
-jQuery.fn.fitElement = function(maxWidth, maxHeight, keepAspect){
+jQuery.fn.tailorfit = function(maxWidth, maxHeight, keepAspect){
 
-	keepAspect = keepAspect || true;
+	if(keepAspect == undefined) keepAspect = true;
+	if(maxWidth   == undefined) maxWidth = 0;
+	if(maxHeight  == undefined) maxHeight = 0;
+
 	var element = $(this);
-	var container = element.parent();
+	var container = $(this).parent();
+
+	element.css('position', 'relative');
 
 	$(window).load(function(){
 		onResize();
 	});
 
-    $(window).resize(function(){
-    	onResize();
+	$(window).resize(function(){
+		onResize();
 	});
+
+	onResize();
 
 	function onResize(){
 
-		var containerHeight = container.height();
-		var containerWidth = container.width();
-		var maxRatio = maxWidth / maxHeight;
-		var containerRatio = containerWidth / containerHeight;
+		var containerHeight = container.outerHeight(true);
+		var containerWidth = container.outerWidth(true);
 
 		var x, y, w, h;
+
+		if(maxWidth == 0) maxWidth = containerWidth;
+		if(maxHeight == 0) maxHeight = containerHeight;
+
 
 		if(!keepAspect){
 			if(containerWidth < maxWidth){
 				x = 0;
-				y = (containerHeight - maxHeight) / 2;
 				w = containerWidth;
-				h = maxHeight;
-			} else if(containerHeight < maxHeight){
-				x = (containerWidth - maxWidth) / 2;
-				y = 0;
-				w = maxWidth;
-				h = containerHeight;
 			} else {
 				x = (containerWidth - maxWidth) / 2;
-				y = (containerHeight - maxHeight) / 2;
 				w = maxWidth;
+			}
+			
+			if(containerHeight < maxHeight){
+				y = 0;
+				h = containerHeight;
+			} else {
+				y = (containerHeight - maxHeight) / 2;
 				h = maxHeight;
 			}
 		} else if(containerWidth >= maxWidth && containerHeight >= maxHeight){
@@ -53,29 +61,26 @@ jQuery.fn.fitElement = function(maxWidth, maxHeight, keepAspect){
 				y = (containerHeight - maxHeight) / 2;
 				w = maxWidth;
 				h = maxHeight;
-		} else if(maxRatio > containerRatio){ // too wide
+		} else {
+			var maxRatio = maxWidth / maxHeight;
+			if(maxRatio > containerWidth / containerHeight){
 				var rh = containerWidth / maxRatio;
 				x = 0;
 				y = (containerHeight - rh) / 2;
 				w = containerWidth;
 				h = rh;
-		} else {
+			} else {
 				var rw = containerHeight * maxRatio;
 				x = (containerWidth - rw) / 2;
 				y = 0;
 				w = rw;
 				h = containerHeight;
-		} 
-
+			} 
+		}
+		
 		element.css({
 			left: x, top: y, width: w, height: h
 		});
 	}
-}
-
-jQuery.fn.fitImage = function(){
-	$(this).load(function(){
-		$(this).fitElement(this.width, this.height, true);
-	});
 }
 
