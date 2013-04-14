861280001a5934ec05c67d33689875abc29b82ef
Rudi Theunisse <groenengoud@gmail.com>
2013-04-15 02:39:53 +1200

    Corrected version number

--- tailorfit-1.2.js
+++ tailorfit-1.2.js
@@ -5,7 +5,7 @@
  * @copyright Rudolf Theunissen 2013
  * @license MIT <http://opensource.org/licenses/mit-license.php>
  * @link https://github.com/rtheunissen/tailorfit
- * @version 1.1.0
+ * @version 1.2
  */
 
  (function( $ ) {
